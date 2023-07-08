832b9f2eb8421f531d0f488575521c147e64e684
Samridhi Gupta <95472912+samridhi07@users.noreply.github.com>
2023-07-08 23:24:55 +0530

    Update streamlit_app.py
    removed snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the data in a table
 streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
